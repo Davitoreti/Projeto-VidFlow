d4c21c9123740f40391fdcb48ab4bbc518ab86ce
Davi Toreti <122990430+Davitoreti@users.noreply.github.com>
2024-03-26 19:31:06 -0300

    criando uma função para mostras os vídeos

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const containerVideos = document.querySelector('.videos__container');
+const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch('http://localhost:3000/videos')
-.then(res => res.json())
-.then((videos) =>
-    videos.forEach((video) => {
+async function buscarEMostrarVideos() {
+    try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
+
+      videos.forEach((video) => {
+        if(video.categoria == ''){
+            throw new Error('Vídeo não tem categoria');
+        }
         containerVideos.innerHTML += `
             <li class="videos__item">
                 <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -13,7 +18,10 @@ const api = fetch('http://localhost:3000/videos')
                     <p class="titulo-canal">${video.descricao}</p>
                 </div>
             </li>
-        `
-    })
-
-)
+        `;
+      })
+    } catch(error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
+    }
+}
+buscarEMostrarVideos();
